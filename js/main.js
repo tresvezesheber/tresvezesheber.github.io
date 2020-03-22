902252211fd06b56bf4d9e3047bdb1c1245055a5
Heber dos Santos Soares de Araujo Lima <desenv2@localhost.localdomain>
2020-03-21 22:56:27 -0300

    paises

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
-axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
-
-axios.get('https://superheroapi.com/api/418828005513547/69')
+axios
+  .get("https://restcountries.eu/rest/v2/all")
   .then(function(response) {
     console.log(response);
+
+    var countries = response.data;
+    var listCountries = document.querySelector("#app ul");
+
+    for (item of countries) {
+      var itemCountry = document.createElement("li");
+      var flagCountry = document.createElement("img");
+      flagCountry.setAttribute('src', item.flag)
+
+      itemCountry.appendChild(flagCountry)
+      var textItem = document.createTextNode(item.name);
+      itemCountry.appendChild(textItem);
+      listCountries.appendChild(itemCountry);
+    }
   })
   .catch(function(error) {
     console.warn(error);
